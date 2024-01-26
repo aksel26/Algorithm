b3890edebbc072d22e8292026147b4e2298424d6
김현민 <kevinxkim2023@gmail.com>
2024-01-26 10:54:48 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,10 +1,2 @@
 def solution(my_string):
-
-    answer = ""
-    for x in my_string:
-        if x.islower() :
-            answer += x.upper()
-        else :
-            answer += x.lower()
-        
-    return answer
+   return my_string.swapcase()
