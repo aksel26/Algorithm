ad3f3f3e6fdecf2b41b7b0ff4eb99fd5505082a3
김현민 <kevinxkim2023@gmail.com>
2025-09-30 18:00:52 +0900

    [Silver IV] Title: 카드2, Time: 328 ms, Memory: 72904 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.js
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -3,17 +3,16 @@ const input = require("fs")
   .toString()
   .trim();
 
-// Node 클래스 - 각 노드는 데이터, 이전 노드, 다음 노드에 대한 참조를 가짐
+// Node 클래스 - 각 노드는 데이터와 다음 노드에 대한 참조를 가짐
 class Node {
   constructor(data) {
     this.data = data;
-    this.prev = null;
     this.next = null;
   }
 }
 
-// DoublyLinkedList 클래스
-class DoublyLinkedList {
+// LinkedList 클래스
+class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -28,54 +27,25 @@ class DoublyLinkedList {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      newNode.prev = this.tail;
       this.tail.next = newNode;
       this.tail = newNode;
     }
     this.size++;
   }
 
-  // 특정 인덱스의 노드 삭제
-  removeAt(index) {
-    if (index < 0 || index >= this.size) {
-      return null;
-    }
+  // 첫 번째 노드 삭제 및 데이터 반환
+  removeFirst() {
+    if (!this.head) return null;
 
-    let current = this.head;
-
-    // 첫 번째 노드 삭제
-    if (index === 0) {
-      this.head = current.next;
-      if (this.head) {
-        this.head.prev = null;
-      } else {
-        this.tail = null;
-      }
+    const data = this.head.data;
+    this.head = this.head.next;
+    
+    if (!this.head) {
+      this.tail = null;
     }
-    // 마지막 노드 삭제
-    else if (index === this.size - 1) {
-      current = this.tail;
-      this.tail = current.prev;
-      this.tail.next = null;
-    }
-    // 중간 노드 삭제
-    else {
-      let count = 0;
-      while (count < index) {
-        current = current.next;
-        count++;
-      }
-      current.prev.next = current.next;
-      current.next.prev = current.prev;
-    }
-
+    
     this.size--;
-    return current.data;
-  }
-
-  // 리스트가 비었는지 확인
-  isEmpty() {
-    return this.size === 0;
+    return data;
   }
 
   // 리스트 크기 반환
@@ -89,16 +59,15 @@ class DoublyLinkedList {
   }
 }
 
-const cards = new DoublyLinkedList();
+const cards = new LinkedList();
 
 for (let i = 1; i <= Number(input); i++) {
   cards.append(i);
 }
 
-while (cards.getSize() !== 1) {
-  cards.removeAt(0);
-  cards.append(cards.getFirst());
-  cards.removeAt(0);
+while (cards.getSize() > 1) {
+  cards.removeFirst();
+  cards.append(cards.removeFirst());
 }
 
 console.log(cards.getFirst());
